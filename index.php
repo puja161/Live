b30b361f60842606b33d7bc4099c52fd456f95b4
puja161 <puja.kumari606@gmail.com>
2014-10-21 16:45:56 +0530

    checking database

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$con=mysql_connect("db548709196.db.1and1.com","dbo548709196","mynewdatabase")or die(mysql_error());
+mysql_select_db("db548709196",$con) or die(mysql_error());
+
+?>
+<html>
+<body>
+Welcome to PACE at TEKNOPLANT
+<BR> Choose right path to proceed
+Checking my website
+</body>
+</html>
